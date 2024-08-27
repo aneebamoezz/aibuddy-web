f7dc4b28711d0434f5db0e7f344ecef13d72bcf2
aneebamoezz <anibakaleem@gmail.com>
2024-08-28 04:45:16 +0500

    Update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      color:{
+       "bg-button": "#00112e"
+      },
+      spacing:{
+        "big":"48rem"
+      }
+    },
+    screens:{
+      sm:"480px",
+      md:"768px",
+      lg:"1024px",
+      xl:"1280px",
+    },
+    fontFamily:{
+      nunito:['Nunito', 'sans-serif']
+    }
   },
   plugins: [],
 }
