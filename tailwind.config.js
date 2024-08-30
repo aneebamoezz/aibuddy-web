1890f766e30d795de60c1385691c0cae772f5a0b
aneebamoezz <anibakaleem@gmail.com>
2024-08-30 17:43:55 +0500

    Update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,21 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
+      animation:{
+        "loop-scroll": "loop-scroll 15s linear infinite"
+      },
+      keyframes:{
+        "loop-scroll": {
+          from: { transform: "translateX(0)"},
+          to: { transform: "translateX(-100%)"},
+        }
+      },
       colors:{
        "bg-button": "#00112e",
        "peach": "#f0b27a",
-       "gray-color": "#475467"
+       "gray-color": "#475467",
+       "skyblue": "#f2f4f7",
+       "orangge": "#e67e22",
       },
       spacing:{
         "big":"48rem"
